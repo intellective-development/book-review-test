c2cd4faea7b3e1b8ed0bc831db18c82a2730505b
diori <intellective.dev@gmail.com>
2017-11-07 13:35:45 -0500

    Initial
    Initial push

---
+++ index.js
@@ -0,0 +1,30 @@
+var request = require('request');
+var parser = require('xml2json');
+var path = require('path');
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+	res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+
+app.get('/search/:keyword', function(req, res){
+
+	//res.end(req.query.keyword);
+
+	var options = {
+		url: 'https://www.goodreads.com/search/index.xml?page=1&key=RDfV4oPehM6jNhxfNQzzQ&q='+req.params.keyword,
+	};
+
+
+	request(options, function (error, response, body) {
+		var json = parser.toJson(body);
+		res.end(json);
+	});
+
+});
+
+app.listen(3000, function() {
+	console.log('App listening on port 3000!')
+});
